5fb3f9e4b70dc002b6fb9780ac4f52db90601266
Javier Jaramillo <ingenierodesoftware73@gmail.com>
2020-11-03 23:14:22 -0500

    create exercises about function as, array_push for add element, array_pop to delete a last element.

--- 14-arrays/functions.php
+++ 14-arrays/functions.php
@@ -1,6 +1,9 @@
 <?php
 
 $singers = ['2pac', 'Drake', 'Jenifer Lopes', 'Alfredo'];
+$numbers = [1, 2, 3, 4, 5];
+sort($numbers);
+var_dump($numbers);
 
-asort($singers);
+array_push($singers, "five turn");
 var_dump($singers);
