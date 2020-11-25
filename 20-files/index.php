21f1ba974238f1bdd2b073eec8b5ad99574a21ca
Javier Jaramillo <ingenierodesoftware73@gmail.com>
2020-11-25 17:57:21 -0500

    trying if exist a file

--- 20-files/index.php
+++ 20-files/index.php
@@ -14,4 +14,18 @@ fwrite($open_file, "<br> add fourth line");
 //close file
 fclose($open_file);*/
 
-copy('text_file.txt', 'file_copy.txt') or die("error al copiar");
+// copy files
+//copy('text_file.txt', 'file_copy.txt') or die("error al copiar");
+
+//rename files
+//rename('file_copy.txt', 'file_copy1.txt');
+
+//delete file
+//unlink('file_copy.txt') or die("Error to delete file");
+
+//try if one file exist
+if (file_exists('text_file1.txt')){
+    echo "the file exists";
+}else{
+    echo "the file doesn't exist";
+}
